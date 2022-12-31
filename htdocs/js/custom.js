d660cd6ce60ea83d7f0c80aa1cf05bc8d9d64617
dinitha <ldinitha@gmail.com>
2022-12-31 21:22:21 +0530

    dinitha 001

---
+++ htdocs/js/custom.js
@@ -0,0 +1,34 @@
+// to get current year
+function getYear() {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    document.querySelector("#displayYear").innerHTML = currentYear;
+}
+
+getYear();
+
+
+
+// slick slider
+$('.chocolate_container').slick({
+    infinite: true,
+    center: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [{
+            breakpoint: 991,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+
+    ]
+});
